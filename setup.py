00e84753d9acc898157a7c927a90741f663abf2b
Sanjay Suthraye <57041731+Sanjay-Suthraye@users.noreply.github.com>
2025-11-27 17:00:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     name="MULTI-AI AGENT",
     version="0.1",
-    author="Sanjay",
+    author="SanjayS",
     packages=find_packages(),
     install_requires = requirements,
 )
